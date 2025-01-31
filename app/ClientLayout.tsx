8084d2aaf6da93ec8494e32f8242bcd2733e735c
Oruchan Asar <oruchan.asar@gmail.com>
2025-01-31 14:31:18 +0300

    move font to layout

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,32 +1,20 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import Navigation from "@/components/Navigation";
 import AuthProvider from "@/components/AuthProvider";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 
-const geistSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-});
-
 export default function ClientLayout({
   children,
+  className,
 }: {
   children: React.ReactNode;
+  className?: string;
 }) {
   return (
-    <body
-      className={`${geistSans.variable} ${geistMono.variable}`}
-      suppressHydrationWarning
-    >
+    <body className={className} suppressHydrationWarning>
       <AuthProvider>
         <ThemeProvider
           attribute="class"
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import ClientLayout from "./ClientLayout";
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
 
 export const metadata: Metadata = {
   title: "Routine Check Extension",
@@ -53,7 +64,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <ClientLayout>{children}</ClientLayout>
+      <ClientLayout className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </ClientLayout>
     </html>
   );
 }
